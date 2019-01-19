7ae5aae8ac92b771edc6986d58d3a523362cf2b9
Hugo Sanchez <hugo@caelumlabs.com>
2019-01-19 12:49:08 +0100

    ran create-react-app and inserted basic text

---
+++ website/src/components/BackgroundImage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Background from '../resources/images/background-image.jpg';
+
+const BackgroundImage = () => {
+    return (
+      <img classname="bg" src={ Background } />
+    );
+}
+
+export default BackgroundImage;
---
+++ website/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="Header">
+            <h1>ETHEREUM CAT HERDERS</h1>
+            <p>
+            Your place to go to get the latest news on upcoming Network Upgrades (Hard Forks),
+            EIP's, and overall community coordination. 
+            </p>
+        </div>
+    );
+}
+
+export default Header;

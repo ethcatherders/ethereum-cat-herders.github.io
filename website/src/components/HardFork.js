d639702d451be14bc1716e853ae647717f4cd8bb
Hugo Sanchez <hugo@caelumlabs.com>
2019-05-13 17:46:56 +0200

    finally updated hardfork section

--- website/src/components/HardFork.js
+++ website/src/components/HardFork.js
@@ -8,28 +8,11 @@ const HardFork = () => {
         <div style={ styles.containerStyle }>
             <h1 style={ styles.titleStyle }>Upcoming Network Upgrade</h1>
             <p style={ styles.textStyle }>
-            On 16 January 2019, a decision was taken to postpone the upgrade of the Ethereum Network, 
-            the upgrade named “Constantinople”, scheduled to complete on 17 December 2019.
+            You can follow everything related to the upcoming Istanbul network upgrade in Ethereum's Wiki:
             </p>
             <br></br>
-            <p style={ styles.textStyle }>
-            The next network upgrade "Constantinople 2" is targeted at <span style={ styles.boldTextStyle }> 27 February 2019</span>, block number <span style={ styles.boldTextStyle }>7.280.000</span>.
-            </p>
             <br></br>
-            <p style={ styles.textStyle }>
-            These are the EIP's that will be implemented:
-            </p>
-            <div> 
-                <p style={ styles.textStyle }><a href="https://github.com/ethereum/EIPs/blob/master/EIPS/eip-145.md">Bitwise shifting instructions in EVM</a></p>
-                <p style={ styles.textStyle }><a href="https://github.com/ethereum/EIPs/blob/master/EIPS/eip-1014.md">Skinny CREATE2</a></p>
-                <p style={ styles.textStyle }><a href="https://github.com/ethereum/EIPs/blob/master/EIPS/eip-1052.md">EXTCODEHASH Opcode</a></p>
-                <p style={ styles.textStyle }><a href="https://github.com/ethereum/EIPs/blob/master/EIPS/eip-1234.md">Delay difficulty bomb, adjust block reward</a></p>
-                <p style={ styles.textStyle }><a href="https://github.com/ethereum/EIPs/blob/master/EIPS/eip-1283.md">Net gas metering for SSTORE without dirty maps</a></p>
-            </div>
-            <br></br>
-            <p style={ styles.textStyle }>
-            All major clients have already implemented these changes. Progres can be tracked <a href="https://github.com/ethereum/pm/wiki/Constantinople-Progress-Tracker">here</a>.
-            </p>
+            <p style={ styles.textStyle }><a href="https://en.ethereum.wiki/roadmap/istanbul">Istanbul Network Upgrade</a></p>
         </div>
     );
 }
